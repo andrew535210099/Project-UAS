3cb4c745074285457e79803ce86b1e5240124d23
andrew535210099 <andrew.535210099@stu.untar.ac.id>
2023-08-02 12:25:09 +0700

    Test dulu

--- server.js
+++ server.js
@@ -67,12 +67,13 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
 
-app.use(function(req, res, next) {
-    res.status(404).render('pages/login');
-  });
 
-  
+app.get("/", function(req, res) {
+    return res.render('pages/login');
+})
+
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
